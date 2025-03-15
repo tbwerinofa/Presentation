7626ae5edc36323be178481078b7ed1e22510322
Tawanda Tafara <tbwerinofa@yahoo.com.au>
2025-03-15 22:23:55 +0200

    Add Swagger

--- Api/Program.cs
+++ Api/Program.cs
@@ -23,6 +23,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json","v1"));
 }
 
 app.UseCors(a=> a.WithOrigins("http://localhost:3000/")
